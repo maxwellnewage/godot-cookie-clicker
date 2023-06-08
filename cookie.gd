b4f2ce584c5525e1eb0d7eda585d4bbecb888b59
Maximiliano Burgos <maxib2007@gmail.com>
2023-06-08 13:00:47 -0300

    cambio en las animaciones de la cookie

--- cookie.gd
+++ cookie.gd
@@ -8,33 +8,22 @@ func _ready() -> void:
 	pressed.connect(_on_pressed)
 	button_down.connect(_on_button_down)
 	button_up.connect(_on_button_up)
-	mouse_entered.connect(_on_mouse_entered)
-	mouse_exited.connect(_on_mouse_exited)
 	
 
 func update_scale() -> void:
-	var pressed_scale: Vector2 = Vector2(1.1, 1.1)
-	var hover_scale: Vector2 = Vector2(0.8, 0.8)
+	var pressed_scale: Vector2 = Vector2(0.7, 0.7)
 	var normal_scale: Vector2 = Vector2(1, 1)
 	animation_tween = create_tween()
 	
 	if button_pressed:
-		animation_tween.tween_property(cookie_texture_rect, "scale", pressed_scale, 0.3).set_trans(Tween.TRANS_ELASTIC)
-	elif is_hovered():
-		animation_tween.tween_property(cookie_texture_rect, "scale", hover_scale, 0.3).set_trans(Tween.TRANS_ELASTIC)
+		animation_tween.tween_property(cookie_texture_rect, "scale", pressed_scale, 0.2).set_trans(Tween.TRANS_SINE)
 	else:
-		animation_tween.tween_property(cookie_texture_rect, "scale", normal_scale, 0.3).set_trans(Tween.TRANS_ELASTIC)
+		animation_tween.tween_property(cookie_texture_rect, "scale", normal_scale, 0.2).set_trans(Tween.TRANS_SINE)
 
-func _on_mouse_entered():
-	update_scale()
-	
 func _on_button_down():
 	update_scale()
-	
+
 func _on_button_up():
-	update_scale()
-	
-func _on_mouse_exited():
 	update_scale()
 
 func _on_pressed():
